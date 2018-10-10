1c6bad1c81eeaa8084bda1a06fcdcb36810c92d5
tabis.jakub <tabis.jakub@gmail.com>
2018-10-10 19:44:07 +0200

    cwiczenia1_3

---
+++ Ćwiczenia1/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//~~~~~~~~~~~~~~~~zadanie 1~~~~~~~~~~~~~~~~~~~~
+
+        int first = 15;
+        int second = 14;
+
+        if ((first >= 13 && first <= 19) && (second < 13 || second > 19)) {
+            System.out.println("teen");
+        } else if ((second >= 13 && second <= 19) && (first < 13 || first > 19)) {
+            System.out.println("teen");
+        }
+
+//~~~~~~~~~~~~~~~~zadanie 2~~~~~~~~~~~~~~~~~~~~
+//
+//        int a = 13;
+//        int b = 17;
+//        int c = 15;
+//        int sum = 0;
+//
+//        if (a == 13){
+//            sum = 0;
+//        } else if (b == 13){
+//            sum = a;
+//        } else if (c == 13){
+//            sum = a + b;
+//        } else {
+//            sum = a + b + c;
+//        }
+//
+//        System.out.println(sum);
+//
+//
+
+//~~~~~~~~~~~~~~~~zadanie 3~~~~~~~~~~~~~~~~~~~~
+//
+//
+//          int[] table = {2, 9, 6, 15, 7, 3};
+//
+//              if((table[i] == 1) || (table[i] == 2) || (table[i] == 3)) {
+//                  System.out.println("True");
+//              }else
+//                  System.out.println("False");
+
+    }
+}
